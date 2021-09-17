ae6c9293f781d02d7afddadf2c765856f9fff5a3
Francisco Mata <fmata@viewnext.com>
2021-09-17 16:14:11 +0200

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -53,10 +54,62 @@ const login = async (req, res = response) => {
         })  
     }
 
-   
+}
+
+
+const googleSingin = async (req, res = response) => {
+
+    const { id_token } = req.body;
+    
+    try {
+
+        const {correo, nombre, img} = await googleVerify(id_token);
+        
+        let usuario = await Usuario.findOne({correo});
+
+        if( !usuario ){
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario (data);
+            await usuario.save();
+        }
+
+        if( !usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+
+        res.json({
+             msg: 'Todo ok! google sigin',
+             googleUser
+        }); 
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        })
+    }
+
+
 
 }
 
 module.exports = {
-    login
+    login,
+    googleSingin
 }
